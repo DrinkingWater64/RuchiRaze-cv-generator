ccb6526c2256ab56c93431c0f34d51869885814f
GuessWhatBBQ <saminslm@gmail.com>
2023-03-19 04:12:25 +0600

    fix: fixed jwt bug

--- server/routes/authentication.js
+++ server/routes/authentication.js
@@ -23,10 +23,15 @@ router.post("/login", async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await User.findOne({ email, password });
-    const accessToken = jwt.sign(user, process.env.JWT_TOKEN, {
-      expiresIn: "5d",
-    });
-    res.json({ message: "ok", accessToken });
+    if (user) {
+      const accessToken = jwt.sign({ email }, process.env.JWT_TOKEN, {
+        expiresIn: "5d",
+      });
+      res.setHeader("Authorization", accessToken);
+      res.json({ message: "ok", accessToken });
+    } else {
+      res.json({ message: "no such user" });
+    }
   } catch (error) {
     res.json(error);
     console.error(error);
